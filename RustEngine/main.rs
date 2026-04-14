934883dcce1b4bce6766e38704820a4713a67583
SA-Dev <seifali6749@gmail.com>
2026-04-14 23:51:15 +0200

    Update main.rs

--- RustEngine/main.rs
+++ RustEngine/main.rs
@@ -1,1 +1,15 @@
-fn main() {}
+mod wrapper_test_2;
+
+use wrapper_test_2::{encode, decode};
+
+fn main() {
+
+    let encoded = encode("Test");
+
+    println!("Encoded size: {}", encoded.size);
+
+    let decoded = decode(encoded);
+
+    println!("Decoded text: {}", decoded);
+
+}
